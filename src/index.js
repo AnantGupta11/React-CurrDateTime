f3d10abb396b182661956d783b16eb64039c72ac
Anant8002749 <ak8002749@gmail.com>
2020-09-19 13:23:42 +0530

    Added map function

--- src/index.js
+++ src/index.js
@@ -5,47 +5,25 @@ import Sdata from './Sdata';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// const currDate= new Date().toLocaleDateString();
-// const currTime= new Date().toLocaleTimeString();
+
+function ncard(val){
+    return(
+      <Card 
+      imgsrc={val.imgsrc}
+      category={val.category}
+      title={val.title}
+      link={val.link}
+      />
+    );
+}
+
+
 ReactDOM.render(
 
   <>
-    <Card 
-    imgsrc={Sdata[0].imgsrc}
-    category={Sdata[0].category}
-    title={Sdata[0].title}
-    link={Sdata[0].link}
-    />  
-    <Card 
-    imgsrc={Sdata[1].imgsrc}
-    category={Sdata[1].category}
-    title={Sdata[1].title}
-    link={Sdata[1].link}
-    />
-    <Card 
-    imgsrc={Sdata[2].imgsrc}
-    category={Sdata[2].category}
-    title={Sdata[2].title}
-    link={Sdata[2].link}
-    />
-    <Card 
-    imgsrc={Sdata[3].imgsrc}
-    category={Sdata[3].category}
-    title={Sdata[3].title}
-    link={Sdata[3].link}
-    />
-    <Card 
-    imgsrc={Sdata[4].imgsrc}
-    category={Sdata[4].category}
-    title={Sdata[4].title}
-    link={Sdata[4].link}
-    /> 
-
+    {Sdata.map(ncard)}
   </>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
